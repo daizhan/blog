5ed79672ffb556b76e478c39479422e6632a4902
daizhan <daizhan@live.cn>
2014-12-04 02:00:33 +0800

    兼容开发环境静态文件和上传文件的访问

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
 
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from blog.settings import DEBUG
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,3 +19,9 @@ urlpatterns = patterns('',
 
     url(r'^$', 'blog.views.test', name="test"),
 )
+
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
