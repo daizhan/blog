ec85a45d2eb97bf20682feb7775b6b020d357bfe
daizhan <daizhan@live.cn>
2015-05-02 12:55:06 +0800

    静态模板练习 petshop

--- blog/urls.py
+++ blog/urls.py
@@ -20,6 +20,12 @@ urlpatterns = patterns('',
     url(r'^$', 'blog.views.test', name="test"),
     url(r'^wangkui/$', 'blog.views.wangkui', name="wangkui"),
     url(r'^wix/stunning/$', 'blog.views.wix_stunning', name="wix-stunning"),
+    url(r'^wix/petshop/index/$', 'blog.views.wix_petshop_index', name="wix-petshop-index"),
+    url(r'^wix/petshop/mart/$', 'blog.views.wix_petshop_mart', name="wix-petshop-mart"),
+    url(r'^wix/petshop/aboutus/$', 'blog.views.wix_petshop_aboutus', name="wix-petshop-aboutus"),
+    url(r'^wix/petshop/blog/$', 'blog.views.wix_petshop_blog', name="wix-petshop-blog"),
+    url(r'^wix/petshop/guide/$', 'blog.views.wix_petshop_guide', name="wix-petshop-guide"),
+    url(r'^wix/petshop/contact/$', 'blog.views.wix_petshop_contact', name="wix-petshop-contact"),
 )
 
 if DEBUG:
