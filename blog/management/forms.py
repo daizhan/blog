1b62c6cf60c06459020c0ca8a64629b926a8889b
daizhan <daizhan@live.cn>
2015-07-24 02:40:56 +0800

    seo 体系构建，完成管里后台信息存储和编辑

--- blog/management/forms.py
+++ blog/management/forms.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from django.core.urlresolvers import reverse
 from blog.const.choices import UserType
+from blog.utils.models import generate_url
 
 
 class UserAdminForm(forms.ModelForm):
@@ -12,10 +14,24 @@ class UserAdminForm(forms.ModelForm):
         user_type = cleaned_data['user_type']
         if user_type == UserType.visitor and password:
             raise forms.ValidationError(
-                '%s 不能设置密码' % UserType.values_to_labels[user_type]
+                u'%s 不能设置密码' % UserType.values_to_labels[user_type]
             )
         elif user_type != UserType.visitor and not password:
             raise forms.ValidationError(
-                '%s 需要设置密码' % UserType.values_to_labels[user_type]
+                u'%s 需要设置密码' % UserType.values_to_labels[user_type]
+            )
+        return cleaned_data
+
+
+class SEOAdminForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super(SEOAdminForm, self).clean()
+        url_name = cleaned_data['url_name']
+        values = cleaned_data['values']
+        url = generate_url(url_name, values)
+        if not url:
+            raise forms.ValidationError(
+                u'url名称(%s)与url值(%s)不匹配' % (url_name, values)
             )
         return cleaned_data
--- blog/repository/admin.py
+++ blog/repository/admin.py
@@ -3,13 +3,16 @@
 from django.contrib import admin
 
 import models
+from blog.management.models import SEORecord
 from blog.utils.models import img_thumb, get_create_time
 from blog.repository.forms import StaticPageAdminForm
+from blog.const.choices import StuffType
 
 
 class TagAdmin(admin.ModelAdmin):
     list_per_page = 20
-    list_display = ['slug', 'name', 'desc', img_thumb, 'count', 'status', get_create_time]
+    list_display = ['slug', 'name', 'desc', img_thumb, 'count',
+                    'status', get_create_time]
     list_filter = ['status']
     search_fields = ['name', 'desc']
 
@@ -19,8 +22,8 @@ admin.site.register(models.Tag, TagAdmin)
 class StuffAdmin(admin.ModelAdmin):
     list_per_page = 20
     list_display = [
-        'slug', 'name', 'desc', img_thumb, 'type', 'access_count', 'comment_count',
-        'good_count', 'verify', 'status', get_create_time
+        'slug', 'name', 'desc', img_thumb, 'type', 'access_count', 
+        'comment_count', 'good_count', 'verify', 'status', get_create_time
     ]
     raw_id_fields = ['tags']
     list_filter = ['type', 'verify', 'status']
@@ -33,13 +36,29 @@ admin.site.register(models.Stuff, StuffAdmin)
 class StaticPageAdmin(admin.ModelAdmin):
     list_per_page = 20
     list_display = [
-        'stuff', 'label', 'url_code', 'desc', 'template', 'verify', 'status', get_create_time
+        'stuff', 'label', 'url_code', 'desc', 'template',
+        'verify', 'status', get_create_time
     ]
     raw_id_fields = ['stuff']
     list_filter = ['verify', 'status']
     search_fields = ['label', 'desc']
 
     form = StaticPageAdminForm
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        if not change: # 首次创建插入seo信息
+            url_name_dict = {
+                StuffType.static_page: 'repository_template_content',
+                StuffType.exercise: 'repository_exercise_content',
+            }
+            stuff_type = obj.stuff.type
+            values = obj.stuff.slug
+            if stuff_type == StuffType.static_page:
+                values += "," + obj.url_code
+            seo = SEORecord(url_name=url_name_dict[obj.stuff.type],
+                            values=values, title=obj.label)
+            seo.save()
 
 admin.site.register(models.StaticPage, StaticPageAdmin)
 
@@ -55,7 +74,8 @@ admin.site.register(models.Comment, CommentAdmin)
 
 class ReplyAdmin(admin.ModelAdmin):
     list_per_page = 20
-    list_display = ['slug', 'content', 'user', 'to_reply', 'comment', 'status', get_create_time]
+    list_display = ['slug', 'content', 'user', 'to_reply', 'comment',
+                    'status', get_create_time]
     list_filter = ['status']
     search_fields = ['content']
 
